ac8584b6745b63da4bfbdeeb536e41edef62372b
ibrahima-bah <71871112+ibrahima-bah@users.noreply.github.com>
2020-11-02 15:55:33 +0100

    premier exercices

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+	$str ="LaPlateforme";
+	$str2="Vive";
+	$str3="!";
+	echo $str2." ".  $str." ". $str3;
+?>
+<br>
+<?php
+	$val = 6;
+	echo $val;
+?>
+<br>
+<?php
+	$val = $val + 4;
+	echo $val;
+?>
+<br>
+<?php
+	$myBool = true;
+	echo $myBool;
+
+?>
